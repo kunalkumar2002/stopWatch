b0c12b1e3efc8dcfdc1937d1175a75dc09d417fb
Kunal Kumar <kunalkumar7091679623@gmail.com>
2023-03-11 03:27:30 +0530

    first time

---
+++ stop.js
@@ -0,0 +1,51 @@
+console.log("hello")
+let second = 0;
+let minutes = 0;
+let hours = 0;
+
+let id = null;
+
+let timer = document.getElementsByClassName('timer');
+let start = document.getElementById('start');
+let pause = document.getElementById('stop');
+let reset = document.getElementById('reset');
+
+function display(){
+     second += 1;
+     if(second === 60){
+        second = 0;
+        minutes++;
+        if(minutes === 60){
+            minutes = 0;
+            hours++;
+        }
+     }
+     timer[0].innerHTML = `${hours} : ${minutes} : ${second}`;  
+}
+
+start.addEventListener('click',function(){
+    if(id !== null){
+        clearInterval(id);
+    }
+    id = setInterval(display , 1000);
+    
+    console.log(id);
+
+});
+
+pause.addEventListener('click',function(){
+    //id = 1;
+    clearInterval(id);
+    //id = true;
+
+})
+
+reset.addEventListener('click',function(){
+    //id = 1;
+    //id = true;
+    clearInterval(id);
+    second = 0;
+    minutes= 0;
+    hours = 0;
+    timer[0].innerHTML = '00 : 00 : 00';
+});
