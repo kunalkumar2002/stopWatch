d8785064531d1d1a2fc4389cfe4287c91f87a374
Kunal Kumar <kunalkumar7091679623@gmail.com>
2023-03-13 19:14:20 +0530

    third commit

--- stop.js
+++ stop.js
@@ -3,52 +3,108 @@ let second = 0;
 let minutes = 0;
 let hours = 0;
 
+let stateOfWatch = "Not Running";
 let id = null;
+let count = 0;
 
-let timer = document.getElementsByClassName('timer');
+let timer = document.getElementsByClassName('time');
 let start = document.getElementById('start');
-let pause = document.getElementById('stop');
+let stop = document.getElementById('stop');
 let reset = document.getElementById('reset');
+let lapcount = document.getElementById('count')
+let laplist = document.getElementById('lap-list')
 
-function display(){
-     second += 1;
-     if(second === 60){
+//handling start button functionality through function
+function timerdisplay(){
+    second++;
+    let s = second < 10 ? '0' + second: second;
+    timer[2].innerHTML = `${s}`;
+    if(second === 60){
         second = 0;
         minutes++;
+        let m = minutes < 10 ? '0' + minutes:minutes;
+        timer[1].innerHTML = `${m} :`
         if(minutes === 60){
             minutes = 0;
             hours++;
+            let h = hours < 10 ? '0' + hours : hours;
+            timer[0].innerHTML = `${h} :`
         }
-     }
-     let h = hours < 10 ? "0" + hours : hours;
-     let m = minutes < 10 ? "0" + minutes : minutes;
-     let s = second < 10 ? "0" + second : second;
-     timer[0].innerHTML = `${h} : ${m} : ${s}`;  
+    }
+}
+
+//massage handler function()
+
+function massagehandler(text){
+    alert(text);
 }
 
 start.addEventListener('click',function(){
+    if(stateOfWatch === "Not Running" || stateOfWatch === "Stopped"){ 
+        id = setInterval(timerdisplay,1000);
+        //marking watch is running;
+        stateOfWatch = "Running";
+        
+    }
+    else{
+        //poping alert it,s already running
+        massagehandler('timer is already running');
+    }
+    count++;
+    
+})
+
+//handling stop button 
+
+stop.addEventListener('click',function(){
     if(id !== null){
         clearInterval(id);
     }
-    id = setInterval(display , 1000);
+    //after stop button clicked making state of watch is stoped
+    stateOfWatch = 'Stopped';
+    //changing html text if start button to resume
+    start.innerText = 'resume';
+
+    //printing lap count;
+    let c = count < 10 ? '0' + count:count;
+    lapcount.innerHTML = `${c}`;
     
-    console.log(id);
+    //calling runtime() to check runtime of lap
+    lapruntime();
+})
 
-});
+//handling reset button
 
-pause.addEventListener('click',function(){
-    //id = 1;
+reset.addEventListener('click' , function(){
     clearInterval(id);
-    //id = true;
+    second = 0;
+    minutes = 0;
+    hours = 0;
+    //making start ineer text agin resume to start
+    start.innerText= 'Start';
+    //making state of watch is not running
+    stateOfWatch = "Not Running";
+    //change to the initial state of hours,minutes and second
+    timer[0].innerHTML = '00 :';
+    timer[1].innerHTML = '00 :'
+    timer[2].innerHTML = '00';
+    //making lap count again 0
+    lapcount.innerHTML = `${0}`;
+    //making lap list innerhtml again 0;
+    laplist.innerHTML = '';
 
 })
 
-reset.addEventListener('click',function(){
-    //id = 1;
-    //id = true;
-    clearInterval(id);
-    second = 0;
-    minutes= 0;
-    hours = 0;
-    timer[0].innerHTML = '00 : 00 : 00';
-});
+//checking how many tme taken in a Lap
+function lapruntime(){
+    
+    laplist.innerHTML = '';
+    const li = document.createElement('li');
+    if(hours === 0){
+        li.innerHTML = `Lap Number ${count} at ${minutes}min : ${second}sec`;
+    }else{
+        li.innerHTML = `Lap Number ${count} at ${hours}hr : ${minutes}min : ${second}sec`;
+    }
+    
+    laplist.append(li);
+}
